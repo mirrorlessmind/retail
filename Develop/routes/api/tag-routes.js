6868d04c34bc29a0519b03c2d2a4bdcc3cb8f9d8
Cigi Tipton <cigi@mirrorlessmind.com>
2021-05-13 18:27:56 -0500

    Update tag-routes.js
    updated the requested tags.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,17 +7,17 @@ router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 
-try {
-  Tag.findAll(
-    {
-      include: {
-        model: Product
+  try {
+    Tag.findAll(
+      {
+        include: {
+          model: Product
+        }
       }
-    }
-  )
-  .then(tagData => res.json(tagData))
-}
-  catch(err) {
+    )
+      .then(tagData => res.json(tagData))
+  }
+  catch (err) {
     res.status(500).json(err);
   }
 });
@@ -25,18 +25,62 @@ try {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findUno({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+    }
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      res.status(500).json(err);
+    })
+
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create(req.body)
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag not found with requested id' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      req.status(500).json(err);
+    })
 });
-
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
